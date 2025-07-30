90937fa10ea5e8d7866c1e73b4a3e4100cb4545f
Quikler <romuchbinyk228@gmail.com>
2025-07-30 17:45:11 +0300

    feat: Create Result type in Common project

---
+++ src/Common/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace Common;
+
+public readonly struct Result<TValue, TError>
+{
+    private readonly TValue? _value;
+    private readonly TError? _error;
+
+    private Result(TValue value)
+    {
+        IsError = false;
+        _value = value;
+        _error = default;
+    }
+
+    private Result(TError error)
+    {
+        IsError = true;
+        _value = default;
+        _error = error;
+    }
+
+    public bool IsError { get; }
+    public bool IsSuccess => !IsError;
+
+    public static implicit operator Result<TValue, TError>(TValue value) => new(value);
+    public static implicit operator Result<TValue, TError>(TError error) => new(error);
+
+    public TResult Match<TResult>(Func<TValue, TResult> success, Func<TError, TResult> failure)
+        => IsSuccess ? success(_value!) : failure(_error!);
+
+    public Task<TResult> MatchAsync<TResult>(Func<TValue, Task<TResult>> success, Func<TError, TResult> failure)
+        => IsSuccess ? success(_value!) : Task.FromResult(failure(_error!));
+}
