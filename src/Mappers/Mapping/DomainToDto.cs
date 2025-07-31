2285f5c5cebb4d3ceefb02747a63e7567753b915
Quikler <romuchbinyk228@gmail.com>
2025-07-31 18:38:27 +0300

    feat: Add Mappers project for all mappings and add it into sln

---
+++ src/Mappers/Mapping/DomainToDto.cs
@@ -0,0 +1,26 @@
+﻿using Common.DTOs;
+using DAL.Entities;
+
+namespace Mappers.Mapping;
+
+public static class DomainToDto
+{
+    public static UserDto ToDto(this UserEntity entity)
+    {
+        return new UserDto
+        {
+            Balance = entity.Balance.ToDto(),
+            Id = entity.Id,
+            Email = entity.Email,
+        };
+    }
+
+    public static BalanceDto ToDto(this BalanceEntity entity)
+    {
+        return new BalanceDto
+        {
+            UserId = entity.UserId,
+            Balance = entity.Balance,
+        };
+    }
+}
