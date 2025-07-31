b33e01825166e6ab4107c6199ca3928be4043c05
Quikler <romuchbinyk228@gmail.com>
2025-07-31 21:52:14 +0300

    feat: Add Balance property to UserResponse

--- src/Contracts/V1/Responses/Account/UserResponse.cs
+++ src/Contracts/V1/Responses/Account/UserResponse.cs
@@ -1,7 +1,10 @@
-﻿namespace Contracts.V1.Responses.Account;
+﻿using Contracts.V1.Responses.Balance;
+
+namespace Contracts.V1.Responses.Account;
 
 public record UserResponse
 {
     public required Guid Id { get; set; }
     public required string Email { get; set; }
+    public required BalanceResponse Balance { get; set; }
 }
