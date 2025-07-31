ea0a47c3e77f81dc41293c2484fdca4b8fa3fdf5
Quikler <romuchbinyk228@gmail.com>
2025-07-31 18:03:26 +0300

    feat: Add FailureDtoExtensions and DtoToApiContract mapping

---
+++ src/WebApi/Extensions/FailureDtoExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using Common.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Mapping;
+
+namespace WebApi.Extensions;
+
+public static class FailureDtoExtensions
+{
+    public static IActionResult ToActionResult(this FailureDto failure)
+    {
+        var response = failure.ToResponse();
+
+        var failureMapping = new Dictionary<FailureCode, IActionResult>
+        {
+            { FailureCode.BadRequest, new BadRequestObjectResult(response) },
+            { FailureCode.Unauthorized, new UnauthorizedObjectResult(response) },
+            { FailureCode.Forbidden, new ForbidResult() },
+            { FailureCode.NotFound, new NotFoundObjectResult(response) },
+            { FailureCode.Conflict, new ConflictObjectResult(response) },
+        };
+
+        return failureMapping.TryGetValue(failure.FailureCode, out var result)
+            ? result
+            : new ObjectResult(response) { StatusCode = StatusCodes.Status500InternalServerError };
+    }
+}
---
+++ src/WebApi/Mapping/DtoToApiContract.cs
@@ -0,0 +1,28 @@
+﻿using Common.DTOs;
+using Contracts.V1.Responses;
+using Contracts.V1.Responses.Account;
+
+namespace WebApi.Mapping;
+
+public static class DtoToApiContract
+{
+    public static FailureResponse ToResponse(this FailureDto failureDto) => new(failureDto.Errors);
+
+    public static AuthResponse ToResponse(this AuthSuccessDto authDto)
+    {
+        return new AuthResponse
+        {
+            Token = authDto.Token,
+            User = authDto.User.ToResponse(),
+        };
+    }
+
+    public static UserResponse ToResponse(this UserDto userDto)
+    {
+        return new UserResponse
+        {
+            Id = userDto.Id,
+            Email = userDto.Email,
+        };
+    }
+}
