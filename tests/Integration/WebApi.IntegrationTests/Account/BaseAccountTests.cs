e3fbbee4ab1bcc06060e0ab328a910660e91e03c
Quikler <romuchbinyk228@gmail.com>
2025-08-02 10:27:39 +0300

    test: Add BaseAccountTests base class for all AccountController tests

---
+++ tests/Integration/WebApi.IntegrationTests/Account/BaseAccountTests.cs
@@ -0,0 +1,23 @@
+﻿using Contracts.V1.Requests.Account;
+using WebApi.IntegrationTests.Abstractions;
+
+namespace WebApi.IntegrationTests.Account;
+
+public class BaseAccountTests(IntegrationTestWebApplicationFactory factory) : BaseIntegrationTest(factory)
+{
+    protected const string TestEmail = "this-email-is-only-for-testing-purposes@test.com";
+    protected const string TestPassword = "testtest";
+
+    protected static LoginUserRequest CreateLoginRequest(string email, string password) => new()
+    {
+        Email = email,
+        Password = password,
+    };
+
+    protected static SignupUserRequest CreateSignupRequest(string email, string password, string confirmPassword) => new()
+    {
+        Email = email,
+        Password = password,
+        ConfirmPassword = confirmPassword,
+    };
+}
