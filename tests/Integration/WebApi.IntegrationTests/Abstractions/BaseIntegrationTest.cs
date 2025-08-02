ae7670a17078af5b5b712971bc739ecb1b0274de
Quikler <romuchbinyk228@gmail.com>
2025-08-02 10:26:58 +0300

    test: Add BaseIntegrationTest base class for all other tests

---
+++ tests/Integration/WebApi.IntegrationTests/Abstractions/BaseIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.IntegrationTests.Abstractions;
+
+[Collection("Test collection")]
+public abstract class BaseIntegrationTest : IAsyncLifetime
+{
+    protected IntegrationTestWebApplicationFactory Factory { get; init; }
+    public HttpClient HttpClient { get; init; }
+    protected AppDbContext DbContext { get; init; }
+
+    public BaseIntegrationTest(IntegrationTestWebApplicationFactory factory)
+    {
+        Factory = factory;
+
+        HttpClient = factory.CreateClient();
+
+        var scope = factory.Services.CreateScope();
+        DbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public Task DisposeAsync() => Factory.ResetDatabaseAsync();
+}
