388355f8bd6122f53ed5c0792b588efcdda62c8c
Quikler <romuchbinyk228@gmail.com>
2025-08-02 10:19:26 +0300

    test: Create class which inherits WebApplicationFactory
    - IntegrationTestWebApplicationFactory:
      - Inherit WebApplicationFactory<Program>, implement IAsyncLifetime
      - Add PostgreSqlContainer from TestContainers to intgration test on
        real database
      - Change registered AppDbContext to the one which is containerized
        with TestContainers
      - Add Respawner to reset database state after each test to remove data
        conflict. This functionality can be done by invoking
        ResetDatabaseAsync on this class
      - On initialization factory starts db container, creates httpclient
        and initializes respawner
      - On Dispose factory stops db container

---
+++ tests/Integration/WebApi.IntegrationTests/Abstractions/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.Common;
+using DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Respawn;
+using Testcontainers.PostgreSql;
+
+namespace WebApi.IntegrationTests.Abstractions;
+
+public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithName("root")
+        .WithPassword("root")
+        .WithDatabase("banking_solution")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.Remove(services.Single(s => s.ServiceType == typeof(DbContextOptions<AppDbContext>)));
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(_dbContainer.GetConnectionString()));
+        });
+    }
+
+    private DbConnection _dbConnection = null!;
+    private Respawner _respawner = null!;
+
+    public HttpClient HttpClient { get; private set; } = null!;
+
+    public async Task InitializeRespawnerAsync()
+    {
+        await _dbConnection.OpenAsync();
+        _respawner = await Respawner.CreateAsync(_dbConnection, new RespawnerOptions
+        {
+            DbAdapter = DbAdapter.Postgres,
+            SchemasToInclude = ["public"],
+        });
+    }
+
+    public Task ResetDatabaseAsync() => _respawner.ResetAsync(_dbConnection);
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        _dbConnection = new NpgsqlConnection(_dbContainer.GetConnectionString());
+        HttpClient = CreateClient();
+        await InitializeRespawnerAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+}
