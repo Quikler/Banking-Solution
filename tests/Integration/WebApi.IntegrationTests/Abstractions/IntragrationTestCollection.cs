00c2f0599cb5bd1377d325667468637391f4c39d
Quikler <romuchbinyk228@gmail.com>
2025-08-02 10:26:30 +0300

    test: Add IntegrationTestCollection which implements ICollectionFixture

---
+++ tests/Integration/WebApi.IntegrationTests/Abstractions/IntragrationTestCollection.cs
@@ -0,0 +1,4 @@
+﻿namespace WebApi.IntegrationTests.Abstractions;
+
+[CollectionDefinition("Test collection")]
+public class IntegrationTestCollection : ICollectionFixture<IntegrationTestWebApplicationFactory>;
